3b0534b1779fdc58d03a801530b6097220ab3aae
Hannah Guay <hannah.n.guay@gmail.com>
2022-11-09 10:13:44 -0700

    finalized project

--- scripts-test.js
+++ scripts-test.js
@@ -1,30 +1,21 @@
 var expect = chai.expect;
 
-//logs nothing... need to make the method static or something?
-describe('createDeck', function() {
-    it('should create a new instance of Deck')
-})
+describe('#createDeck', function() {
+    it('should create a new instance of Deck', function(done) {
+        let testDeck = new Deck();
+        testDeck.createDeck();
+        console.log("testDeck:", testDeck)
+        let testDeckKeys = Object.keys(testDeck.deck).length;
+        console.log("testDeck object.keys:", testDeckKeys);
+        expect(testDeckKeys).to.eql(52);
+        done();
+    });
+    
+});
 
-//logs nothing
-expect(myDeck).to.have.lengthOf(52);
 
-//QUESTIONS:
 
-//can you test a function within a class - would you create
-//an instance of the
-//class and then test the method?
 
-//feels like finding a lot of diff syntax
-//ie
-//const expect = chai.expect; v
-//vs
-//const chai = require('chai'),
-//expect = chai.expect;
 
-//where do you put the export module? immediately after 
-//the unit you're testing or at the very end of the file?
 
-//difference between mocha and chai
-
-//difference between assert and expect
 
--- scripts.js
+++ scripts.js
@@ -8,8 +8,9 @@ class Deck {
   }
   //method to create 52 unique cards
   createDeck() {
-    var suits = ["Spade", "Club", "Heart", "Diamond"];
-    var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    var suits = ["Spades", "Clubs", "Hearts", "Diamonds"];
+    var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+   
     //loop through the length of the suits array and length of values array,
     //pairing a suit with a value, calling each pair a newCard and pushing the 
     //pair to the deck array
@@ -37,56 +38,69 @@ class Deck {
   }
 }
 
-//create new instance of Deck
+//create new instance of Deck, call the createDeck and shuffleDeck methods
 let myDeck = new Deck;
 myDeck.createDeck()
 myDeck.shuffleDeck()
-console.log("call createDeck, shuffleDeck methods on myDeck", myDeck);
+// console.log("Create Deck:", myDeck);
 
 //STEP 2: divide the randomized values arrray or object equally between two hands
 
 //create a variable that finds the middle value of the length of the myDeck.deck array
 const middlePoint = Math.ceil(myDeck.deck.length / 2);
 
-//use splice to cut from the first index to the middle point value and put that into the variable playerHand1
+//use splice to cut from the first index to the middle point value and put that into the variable playerOneHand
 let playerOneHand = myDeck.deck.splice(0, middlePoint);
 
-//use splice to cut from the last index to the middle point value and put that into the variable playerHand1
+//use splice to cut from the last index to the middle point value and put that into the variable playerTwoHand
 let playerTwoHand = myDeck.deck.splice(-middlePoint);
 
 console.log("playerOneHand", playerOneHand);
 console.log("playerTwoHand", playerTwoHand);
 
-//STEP 3: pull a single value from each playerhand and compare the two values
-
+//STEP 3: pull a single value from each playerHand and compare the two values
 
 //create variables to hold score count
 let playerOneScore = 0;
-  let playerTwoScore = 0;
+let playerTwoScore = 0;
 
 //this function
 function drawCard() {
- 
+  
+  //this loop iterates through the players' decks 26 times to pull a value from each hand per iteration
+  //compare those values, keep a total score and then log the highest score
   for (let i = 0; i < 26 ; i++) {
-    let playerOneCard = playerOneHand[i].Value;
-    let playerTwoCard = playerTwoHand[i].Value;
-      console.log(`Round # ${i + 1}, Player One Card: ${playerOneCard}, Player Two Card: ${playerTwoCard}`);
-      if(playerOneCard > playerTwoCard) {
+
+    //variables to display the suit and value of each card drawn
+    let playerOneCard = playerOneHand[i].Value + " of " + playerOneHand[i].Suit;
+    let playerTwoCard = playerTwoHand[i].Value + " of " + playerTwoHand[i].Suit;
+
+    //variables that will use the value of i each time through the loop to pull the corresponding
+    //index number of the players' hands
+    let playerOneCardValue = playerOneHand[i].Value;
+    let playerTwoCardValue = playerTwoHand[i].Value;
+
+      //compares the drawn cards' values and adds a point to the playerScore that the higher value belonged to
+      console.log(`Round # ${i + 1}, Player One Card: ${[playerOneCard]}, Player Two Card: ${playerTwoCard}`);
+      if(playerOneCardValue > playerTwoCardValue) {
         playerOneScore++;
         console.log(`Player One Wins! Player One Total Points: ${playerOneScore}`);
-      } else if(playerTwoCard > playerOneCard) {
+      } else if(playerTwoCardValue > playerOneCardValue) {
         playerTwoScore++;
         console.log(`Player Two Wins! Player Two Points: ${playerTwoScore}`);
       } else {
         console.log(`Round # ${i + 1}, Draw`)
       }
   }
+  //after the loop completes, display the final score.
   console.log(`----------------------
   Final Scores: Player One: ${playerOneScore}, Player Two: ${playerTwoScore}`);  
 }
 
-//this function
+//calling drawCard function to start the game
 drawCard();
+
+// STEP 4: compare the final scores for each player and declare the player with highest points the winner.
 
 if(playerOneScore > playerTwoScore) {
   console.log('Player One Wins the Game!');
